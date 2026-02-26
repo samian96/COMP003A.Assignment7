f34b8f3598f18cf3b27f8fdc7a4e4ec94067855e
samian <sam.iannazzo@yahoo.com>
2026-02-25 22:31:21 -0800

    added int check for stock / case 3 for checking sum of int / case 4 to exit program / default for safety

--- COMP003A.Assignment7/Program.cs
+++ COMP003A.Assignment7/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Globalization;
 
 namespace COMP003A.Assignment7
 {
@@ -14,14 +15,17 @@ namespace COMP003A.Assignment7
             products.Add("Adhesive");
 
             int[] stock = { 3, 15, 5, 2, 0 };
+            int sum = 0;
 
             bool running = true;
 
             while (running)
             {
+                Console.WriteLine("\n");
                 Console.WriteLine("1. Display Inventory\n2. Show Total\n3. Show Average\n4. Exit Program");
                 Console.Write("Please enter option: ");
                 string userInput = Console.ReadLine();
+                Console.WriteLine("\n");
 
                 switch (userInput)
                 {
@@ -29,9 +33,34 @@ namespace COMP003A.Assignment7
                         
                         foreach (string product in products)
                         {
-                            Console.WriteLine("\n" + product);
+                            Console.WriteLine(product);
                         }
                         break;
+                    case "2": 
+
+                        for (int i = 0; i < stock.Length; i++)
+                        {
+                            Console.WriteLine(stock[i]);
+                        }
+                        break;
+                    case "3": 
+                        foreach (int number in stock)
+                        {
+                            sum += number;
+                        }
+                        double average = (double)sum / stock.Length;
+
+                        Console.WriteLine("Average is: " + average);
+                        break;
+                    case "4":
+                        Console.WriteLine("End of program.");
+
+                        running = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input. Please try again.");
+                        break;
+
 
                         
                 }
